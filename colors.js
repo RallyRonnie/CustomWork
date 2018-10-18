dc3f274646d6af64f58a8b341d10e8c1969ea84f
RallyRonnie <ronnie.pinkerton@ca.com>
2018-10-18 15:47:52 -0500

    Save colors

---
+++ colors.js
@@ -0,0 +1,111 @@
+// Default custom chart colors
+chartColors: [
+    "#FF8200", // $orange
+    "#F6A900", // $gold
+    "#FAD200", // $yellow
+    "#8DC63F", // $lime
+    "#1E7C00", // $green_dk
+    "#337EC6", // $blue_link
+    "#005EB8", // $blue
+    "#7832A5", // $purple,
+    "#DA1884",  // $pink,
+    "#C0C0C0" // $grey4
+    ];
+// FROm Value vs. Risk and PI Alignment - last .6 is opacity for bubbles
+// light blue, green, gold etc...
+var COLORS = [
+    'rgba(204,  0,  0, .6)', // #cc0000 - dark red
+    'rgba(204,204,  0, .6)', // #cccc00 - lt gold/gr
+    'rgba(  0,204,  0, .6)', // #00cc00 - bright green
+    'rgba(  0,102,204, .6)', // #0066cc - royal blue
+    'rgba(102,  0,204, .6)', // #6600cc - purple
+    'rgba( 96, 96, 96, .6)', // #066060 - teal
+    'rgba(204,102,  0, .6)', // #cc6600 - drk orange
+    'rgba(  0,  0,204, .6)'  // #0000cc - drk blue
+];
+// 
+ChartColors: [
+'#c42525', // drk salmon
+'#a6c96a', // lt olive green
+'#7cb5ec', // lt blue
+'#434348', // drk gray
+'#90ed7d', // lt green
+'#f7a35c', // peach
+'#8085e9', // lt purple
+'#aa1925', // med red
+'#f15c80', // pink
+'#e4d354', // gold
+'#2b908f', // drk teal
+'#f45b5b', // med salmon
+'#91e8e1', // cyan ish
+'#1aadce', // med cyan
+'#4572A7', // steel blue
+'#AA4643', // drk salmon
+'#89A54E', // med olive green
+'#80699B', // med purple
+'#3D96AE', // med teal
+'#DB843D', // lt orange
+'#92A8CD', // lt steel blue
+'#A47D7C', // mauve
+'#B5CA92']; // lt olive
+
+chartColors: [ 
+        '#2f7ed8', // blue
+        '#8bbc21', // lt green
+        '#910000', // dr red
+        '#492970', // purple
+        '#f28f43', // lt orange
+        '#145499', // med blue
+        '#77a1e5', // lt blue
+
+        '#c42525', // lt red
+        '#a6c96a', // lt green
+        '#7cb5ec', // sky blue
+        '#434348', // grey
+        '#90ed7d', // bright green
+        '#f7a35c', // peach
+        '#8085e9',
+        '#aa1925',
+        '#f15c80', 
+        '#e4d354', 
+        '#2b908f', 
+        '#f45b5b', 
+        '#91e8e1',
+        '#1aadce',
+        '#4572A7', 
+        '#AA4643', 
+        '#89A54E', 
+        '#80699B', 
+        '#3D96AE',
+        '#DB843D', 
+        '#92A8CD', 
+        '#A47D7C', 
+        '#B5CA92'];
+
+        // FROM Web vis site colorbrewer2.org
+'#8dd3c7', // Pastels
+'#ffffb3',
+'#bebada',
+'#fb8072',
+'#80b1d3',
+'#fdb462',
+'#b3de69',
+'#fccde5',
+'#d9d9d9',
+'#bc80bd',
+'#ccebc5',
+'#ffed6f',
+
+// Primary
+'#a6cee3',
+'#1f78b4',
+'#b2df8a',
+'#33a02c',
+'#fb9a99',
+'#e31a1c',
+'#fdbf6f',
+'#ff7f00',
+'#cab2d6',
+'#6a3d9a',
+'#ffff99',
+'#b15928',
